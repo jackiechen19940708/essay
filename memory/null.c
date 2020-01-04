ed2d0b72c41cea28724ae4c32249735e23cff17b
c00465427 <c00465427@hisilicon.com>
2020-01-04 16:30:53 +0800

    Update

--- memory/null.c
+++ memory/null.c
@@ -2,6 +2,14 @@
 using namespace std;
 int main(){
     int * i_p = NULL;
-    cout <<*i_p<<endl;
+    //cout <<*i_p<<endl;
+    i_p = (int*) malloc(sizeof(int));
 
+    int * ii_p = NULL;
+    //cout <<*i_p<<endl;
+    ii_p = (int*) malloc(100*sizeof(int));
+    ii_p[100] = 0;
+    //free (ii_p);
+    cout << ii_p[0] <<endl;
+    free (ii_p+50);
 }
