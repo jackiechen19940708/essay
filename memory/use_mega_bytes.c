5cd19c06f3446212b3e86bbf5b338af6f57febf2
c00465427 <c00465427@hisilicon.com>
2020-01-05 19:40:39 +0800

    Read Memory Virtualization Overview

--- memory/use_mega_bytes.c
+++ memory/use_mega_bytes.c
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <cstdlib>
+#include <unistd.h>
 using namespace std;
 int main(int argc,char * argv[]){
     if (argc<2){
         cout <<"Usage ./a.out n, where n are the num of mega bytes you want to use"<<endl;
         return -1;
     }
-    cout << atoi(argv[1])/4<<endl;
+    cout << "process id:" << getpid() << endl;
+    printf("location of code : %p\n", main);
     // lazy use
     int *a = new int[((long long)atoi(argv[1]))/4*1024*1024];
     // only when visit can we see memory usage(free -h) boost
